008ecb183ccc37517fd1913c15e763247d69f869
Diego Alejandro Ceron <diego.ceron@palaceresorts.com>
2024-02-21 23:41:38 -0600

    feat: add navbar

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,91 +1,196 @@
-<script lang="ts" setup>
-import { Button } from "@/components/ui/button"
+<script setup lang="ts">
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import { ref } from "vue";
+
+const showProfileOptions = ref(false);
+
+const clickProfile = () => {
+  showProfileOptions.value = !showProfileOptions.value;
+};
 </script>
 
 <template>
-  <div>Navbar</div>
-
-  <div>
-    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          class="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
-        <h2
-          class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
-        >
-          Sign in to your account
-        </h2>
-      </div>
-
-      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form class="space-y-6" action="#" method="POST">
-          <div>
-            <label for="email" class="block text-sm font-medium leading-6 text-gray-900"
-              >Email address</label
-            >
-            <div class="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autocomplete="email"
-                required
-                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <div class="flex items-center justify-between">
-              <label
-                for="password"
-                class="block text-sm font-medium leading-6 text-gray-900"
-                >Password</label
+  <section>
+    <div class="w-full">
+      <nav class="bg-slate-100 h-24">
+        <div class="mx-auto max-w-10xl py-4 px-8 sm:px-6 lg:px-12">
+          <div class="relative flex h-16 items-center justify-between">
+            <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <!-- Mobile menu button-->
+              <button
+                type="button"
+                class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
               >
-              <div class="text-sm">
-                <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >Forgot password?</a
+                <span class="absolute -inset-0.5"></span>
+                <span class="sr-only">Open main menu</span>
+                <!--
+            Icon when menu is closed.
+
+            Menu open: "hidden", Menu closed: "block"
+          -->
+                <svg
+                  class="block h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
                 >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+                <!--
+            Icon when menu is open.
+
+            Menu open: "block", Menu closed: "hidden"
+          -->
+                <svg
+                  class="hidden h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div
+              class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"
+            >
+              <div class="flex flex-shrink-0 items-center">
+                <img
+                  class="h-14 w-auto"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Palace_Resorts_Brand_Logo.svg/2560px-Palace_Resorts_Brand_Logo.svg.png"
+                  alt="Your Company"
+                />
+               <!--  <span class="pl-4 uppercase font-semibold"> The Palace Company</span> -->
+              </div>
+  
+            </div>
+            <div
+              class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+            >
+              <button
+                type="button"
+                class="relative rounded-full bg-slate-300 p-1 text-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+              >
+                <span class="absolute -inset-1.5"></span>
+                <span class="sr-only">View notifications</span>
+                <svg
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                  />
+                </svg>
+              </button>
+
+              <!-- Profile dropdown -->
+              <div class="relative ml-3">
+                <div>
+                  <button
+                    @click="clickProfile"
+                    type="button"
+                    class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    id="user-menu-button"
+                    aria-expanded="false"
+                    aria-haspopup="true"
+                  >
+                    <span class="absolute -inset-1.5"></span>
+                    <span class="sr-only">Open user menu</span>
+                    <img
+                      class="h-8 w-8 rounded-full"
+                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      alt=""
+                    />
+                  </button>
+                </div>
+                <div
+                  v-if="showProfileOptions"
+                  class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="user-menu-button"
+                  tabindex="-1"
+                >
+                  <!-- Active: "bg-gray-100", Not Active: "" -->
+                  <div class="h-full block text-sm text-gray-700">
+                    <NavigationMenuItem>
+                      <NuxtLink to="/docs" class="!h-full !min-h-full">
+                        <NavigationMenuLink
+                          :class="navigationMenuTriggerStyle()"
+                          class="!h-full !min-h-full"
+                        >
+                          Your Profile
+                        </NavigationMenuLink>
+                      </NuxtLink>
+                    </NavigationMenuItem>
+                  </div>
+                  <div class="h-full block text-sm text-gray-700">
+                    <NavigationMenuItem>
+                      <NuxtLink to="/docs">
+                        <NavigationMenuLink
+                          :class="navigationMenuTriggerStyle()"
+                        >
+                          Settings
+                        </NavigationMenuLink>
+                      </NuxtLink>
+                    </NavigationMenuItem>
+                  </div>
+                  <div class="h-full block text-sm text-gray-700">
+                    <NavigationMenuItem>
+                      <NuxtLink to="/docs">
+                        <NavigationMenuLink
+                          :class="navigationMenuTriggerStyle()"
+                          class="h-full"
+                        >
+                          Sign out
+                        </NavigationMenuLink>
+                      </NuxtLink>
+                    </NavigationMenuItem>
+                  </div>
+                </div>
               </div>
             </div>
-            <div class="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autocomplete="current-password"
-                required
-                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
           </div>
+        </div>
 
-          <div>
-            <button
-              type="submit"
-              class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        <!-- Mobile menu, show/hide based on menu state. -->
+        <div class="sm:hidden" id="mobile-menu">
+          <div class="space-y-1 px-2 pb-3 pt-2">
+            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+            <a
+              href="#"
+              class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+              aria-current="page"
+              >Dashboard</a
             >
-              Sign in
-            </button>
+            <a
+              href="#"
+              class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+              >Team</a
+            >
           </div>
-        </form>
-
-        <p class="mt-10 text-center text-sm text-gray-500">
-          Not a member?
-          <a
-            href="#"
-            class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-            >Start a 14 day free trial</a
-          >
-        </p>
-      </div>
+        </div>
+      </nav>
     </div>
-  </div>
-
-  <div>
-    <Button>Hello World</Button>
-  </div>
+  </section>
 </template>
